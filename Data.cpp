50f2a245f9d499b99b356cd9d7c119ddaa723178
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2023-10-09 13:53:04 -0600

    Container modified. Now instead of being a continuous array, now is a list of arrays. The intension is to reduce the fracmentation of the memory.

--- Data.cpp
+++ Data.cpp
@@ -1,35 +1,78 @@
 #include"Data.hpp"
 #include<iostream>
 
-Data::Data(): content(NULL), size(0) {}
+Data::Data(): content(NULL), last(NULL), size(0) {}
 
 Data::Data(const char source[], unsigned _size) : size(_size) {
-    content = new char[_size];
-    for(unsigned i = 0; i < _size; i++) content[i] = source[i];
+    if(_size == 0 || source == NULL) {
+        content = NULL;
+        return;
+    }
+    unsigned numBlocks = _size >> 10; // numBlocks = _size / 1024
+    unsigned rem = _size & 1023; // rem = _size % 1023
+    unsigned i, j, k;
+    if(rem != 0)     // -size is not a multiple of 1024.
+        numBlocks++; //  Adding an extra block.
+    // Allocating first block.
+    content = new Block;
+    last = content;
+    for(i = 1; i < numBlocks; i++) { // Copying data.
+        k = --i << 10; // k = (i-1) * 1024
+        for(j = 0; j < 1024; j++) {
+            last->bytes[j] = source[j + k];
+        }
+        last->next = new Block;
+        last = last->next;
+    }
+    rem != 0 ? rem : rem = 1024; // Filling the last block.
+    for(j = 0, k += 1024; j < rem; j++) last->bytes[j] = source[j + k];
+    last->next = NULL;
 }
 
 Data::~Data() {
-    if(content != NULL) delete [] content;
+    if(content != NULL) while(content != NULL) {
+        Block* prev = content;
+        content = content->next;
+        delete prev;
+    }
 }
 
 int Data::append(const char source[], unsigned sz) {
     if(sz == 0) return 0; // Nothing to append.
 
-    unsigned _size = size + sz, i = 0xFFFFFFFF;
-    char* _content = new char[_size];
+    size += sz; // Updating size.
+    unsigned numBlocks = sz >> 10; // numBlocks = sz / 1024
+    unsigned rem = sz & 1023;  // rem = sz % 1024
+    unsigned i, j, k = 0;
+    if(rem != 0)     // -sz is not a multiple of 1024.
+        numBlocks++; //  Adding an extra block.
 
-    // Copying the old content.
-    while(++i < size) _content[i] = content[i];
+    // Allocating first extra block.
+    if(content == NULL) { // In case of empty container.
+        content = new Block;
+        last = content;
+    }
+    else {
+        last->next = new Block;
+        last = last->next;
+    }
     // Copying the new content.
-    for(i == 0 ? i : i--; i < _size; i++ ) _content[i] = source[i];
-
-    if(content != NULL) delete [] content;
-    content = _content;
-    size = _size;
+    numBlocks--; // Last block is treated separately
+    for(i = 0; i < numBlocks; i++) {
+        k = i << 10; // k = i * 1024
+        for(j = 0; j < 1024; j++) {
+            last->bytes[j] = source[j + k];
+        }
+        last->next = new Block;
+        last = last->next;
+    }
+    rem != 0 ? rem : rem = 1024;  // Filling the last block.
+    for(j = 0, k = i << 10; j < rem; j++) last->bytes[j] = source[j + k];
+    last->next = NULL;
     return 0;
 }
 
-char* Data::getContent() const {
+Data::Block* Data::getContent() const {
     return content;
 }
 
--- Data.hpp
+++ Data.hpp
@@ -1,24 +1,27 @@
 // -Handling input data.
 // -It is mostly aimed to act as a buffer for large amounts of data that can
 //  be set as input for the Sha512 algorithm.
+#define BLOCK_SIZE 1024
 
 struct Data{
-	private:
-	char* content;
-	// -Size of the data in bytes. Upper limit is 2^128 - 1 bytes.
-	unsigned size;
+	private: struct Block {
+		char bytes[BLOCK_SIZE];
+		Block* next;
+	};
+	Block *content, *last;
+	unsigned size; // -Size of data in bytes. Upper limit is 2^32 - 1.
 
 	public:
 	// Copying bytes from source.
 	Data(const char source[], unsigned _size = 0);
-	// content <- {NULL, NULL}, size <- 0.
-	Data();
-	// Releasing memory.
-	~Data();
+
+	Data(); // content <- {NULL, NULL}, size <- 0.
+
+	~Data(); // Releasing memory.
 
 	// -Appends information at the end of the content.
 	int append(const char source[], unsigned sz);
 
-	char* getContent() const;
+	Block* getContent() const;
 	unsigned getSize() const;
 };
